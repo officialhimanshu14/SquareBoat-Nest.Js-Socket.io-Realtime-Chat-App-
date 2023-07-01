2d878dafc6c4fc632a5da281ea725d3b8beb2859
HARSH VARDHAN GUPTA <85693353+Harnyx-Dope@users.noreply.github.com>
2023-07-01 09:51:44 +0530

    Update ChattingGateways.ts

--- src/ChattingGateways.ts
+++ src/ChattingGateways.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { GlaGateways } from './socket-gateway';
 
 @Injectable()
 export class GlaService {
   private server: Server;
   constructor(private readonly  gateway: GlaGateways) {}
+  joinRoom(socket: Socket, room: string): void {
+    socket.join(room);
+  }
+
+  leaveRoom(socket: Socket, room: string): void {
+    socket.leave(room);
+  }
+
+  sendMessage(socket: Socket, room: string, message: string): void {
+    socket.to(room).emit('message', message);
+  }
 
   setServer(server: Server) {
     this.server = server;
